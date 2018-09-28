f37e031a7a2c93e3ba44cff0f43dd6df55e2b857
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-09-28 20:56:05 +0900

    add install procedure

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # archlinux_install_script
+
+## install procedure
+
+```
+# install core
+wget https://raw.githubusercontent.com/mitubaEX/archlinux_install_script/master/setup.sh
+sh setup.sh
+
+reboot
+
+# please login your root password
+
+# install package
+wget https://raw.githubusercontent.com/mitubaEX/archlinux_install_script/master/setup.sh
+sh setup.sh
+
+reboot
+
+# please login your root user and your create other user
+useradd -m <username>
+passwd <username>
+
+pacman -S sudo
+visudo # Please add `<username> ALL=(All) ALL` below `root ALL=(ALL) ALL` line
+
+reboot
+
+# login your other user and enjoy!
+```
--- package.sh
+++ package.sh
@@ -1,37 +1,29 @@
 #!/bin/sh
 set -ux
 
-CHROOT="arch-chroot /mnt"
-$CHROOT pacman -S python3 python-pip neovim alacritty git tmux zsh vim go
+sudo pacman -S python3 python-pip neovim alacritty git tmux zsh vim go i3 feh compton
 
 # desktop
-$CHROOT pacman -S xorg-server xorg-apps xorg-xinit
-$CHROOT pacman -S mesa
-$CHROOT pacman -S xf86-video-intel
-$CHROOT pacman -S xf86-input-synaptics
-$CHROOT pacman -S xfce4 xfce4-goodies
-$CHROOT pacman -S lightdm lightdm-gtk-greeter
-$CHROOT systemctl enable lightdm.service
+sudo pacman -S xorg-server xorg-apps xorg-xinit mesa xf86-video-intel xf86-input-synaptics xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+sudo systemctl enable lightdm.service
 
-$CHROOT pacman -S otf-ipafont
+sudo pacman -S otf-ipafont fcitx-im fcitx-configtool fcitx-mozc
 
-$CHROOT pacman -S fcitx-im fcitx-configtool fcitx-mozc
+sudo echo "export GTK_IM_MODULE=fcitx" > $HOME/.xprofile
+sudo echo "export QT_IM_MODULE=fcitx" >> $HOME/.xprofile
+sudo echo 'export XMODIFIERS="@im=fcitx"' >> $HOME/.xprofile
 
-$CHROOT echo "export GTK_IM_MODULE=fcitx" > $HOME/.xprofile
-$CHROOT echo "export QT_IM_MODULE=fcitx" >> $HOME/.xprofile
-$CHROOT echo 'export XMODIFIERS="@im=fcitx"' >> $HOME/.xprofile
+sudo pacman -S firefox
 
-$CHROOT pacman -S firefox
+sudo pacman -Sy --noconfirm git base-devel
+sudo git clone https://aur.archlinux.org/google-chrome.git
+sudo cd google-chrome
+sudo makepkg -s --noconfirm
+sudo pacman -U --noconfirm google-chrome-*.pkg.tar.xz
+sudo pacman -Sy --noconfirm ttf-sazanami
 
-$CHROOT pacman -Sy --noconfirm git base-devel
-$CHROOT git clone https://aur.archlinux.org/google-chrome.git
-$CHROOT cd google-chrome
-$CHROOT makepkg -s --noconfirm
-$CHROOT pacman -U --noconfirm google-chrome-*.pkg.tar.xz
-$CHROOT pacman -Sy --noconfirm ttf-sazanami
-
-$CHROOT echo "---------"
-$CHROOT echo "finished."
-$CHROOT echo "---------"
+sudo echo "---------"
+sudo echo "finished."
+sudo echo "---------"
 
 set +x
