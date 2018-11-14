a7fba9f76457669b47526c24d0b8922b80bcb0ed
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-11-14 23:48:56 +0900

    add chromium

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ux
 
-sudo pacman -S python3 python-pip neovim alacritty git tmux zsh vim go i3 feh compton fzf gnome-keyring dmenu pulseaudio pavucontrol xfce4-pulseaudio-plugin pamixer evince texlive-langjapanese texlive-most ghostscript evince poppler-data exa rofi blueberry jdk8-openjdk
+sudo pacman -S python3 python-pip neovim alacritty git tmux zsh vim go i3 feh compton fzf gnome-keyring dmenu pulseaudio pavucontrol xfce4-pulseaudio-plugin pamixer evince texlive-langjapanese texlive-most ghostscript evince poppler-data exa rofi blueberry jdk8-openjdk chromium
 
 # desktop
 sudo pacman -S xorg-server xorg-apps xorg-xinit mesa xf86-video-intel xf86-input-synaptics xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
@@ -15,12 +15,12 @@ sudo echo 'export XMODIFIERS="@im=fcitx"' >> $HOME/.xprofile
 
 sudo pacman -S firefox
 
-sudo pacman -Sy --noconfirm git base-devel
-sudo git clone https://aur.archlinux.org/google-chrome.git
-sudo cd google-chrome
-sudo makepkg -s --noconfirm
-sudo pacman -U --noconfirm google-chrome-*.pkg.tar.xz
-sudo pacman -Sy --noconfirm ttf-sazanami
+# sudo pacman -Sy --noconfirm git base-devel
+# sudo git clone https://aur.archlinux.org/google-chrome.git
+# sudo cd google-chrome
+# sudo makepkg -s --noconfirm
+# sudo pacman -U --noconfirm google-chrome-*.pkg.tar.xz
+# sudo pacman -Sy --noconfirm ttf-sazanami
 
 sudo echo "---------"
 sudo echo "finished."
